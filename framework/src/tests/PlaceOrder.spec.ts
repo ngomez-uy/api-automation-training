71b410aa3d1715a88ababf497a671999f1d7990f
Nicolas Gomez <nicolas.gomez@endava.com>
2025-01-16 12:20:35 -0300

    Changes for PR comments

--- framework/src/tests/PlaceOrder.spec.ts
+++ framework/src/tests/PlaceOrder.spec.ts
@@ -9,7 +9,6 @@ chai.should();
 describe("Add Order", () => {
   const storeService = new StoreService();
   
-
   it("@Smoke - Place Order correctly", async () => {
     const order: OrderModel = {
       id: 2,
@@ -23,20 +22,14 @@ describe("Add Order", () => {
     const response = await storeService.placeOrder<OrderResponse>(order);
     const formattedShipDate = response.data.shipDate.toString().split('T')[0];
     if (!formattedShipDate) throw new Error("Ship Date is not defined");
-    
-
     response.status.should.equal(200, JSON.stringify(response.data));
-    response.data.id.should.be.above(0); //to verify it is a valid id
-    response.data.petId.should.be.above(0); //to verify it is a valid petId
-    response.data.quantity.should.be.above(0); //to verify it is a valid quantity
-    response.data.quantity.should.equal(order.quantity); //to verify it is the same quantity
-    formattedShipDate.should.equal(order.shipDate); //to verify it is the same shipDate
-    response.data.status.should.equal(order.status); //to verify it is the same status
-    response.data.complete.should.equal(order.complete); //to verify it is the same complete
-    console.log(formattedShipDate);
-    
-    
-  
+    response.data.id.should.be.above(0); 
+    response.data.petId.should.be.above(0); 
+    response.data.quantity.should.be.above(0); 
+    response.data.quantity.should.equal(order.quantity); 
+    formattedShipDate.should.equal(order.shipDate); 
+    response.data.status.should.equal(order.status); 
+    response.data.complete.should.equal(order.complete); 
   });
   
 });
